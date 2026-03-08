4a000ff8217330c61e5ac97836c5300c0c9b157e
JCFernandez-data <fernandezjulio.jfs@gmail.com>
2026-03-08 04:39:51 +0000

    Finalizadas las 15 misiones de SQL

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -22,31 +22,62 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- SELEC * FROM opservation LIMIT 10;
+ SELECT * FROM observations LIMIT 10;
 
 -- MISSION 2
--- SELECT DISTINCS region_id FROM opservation; 
+ SELECT DISTINCT region_id FROM observations; 
 
 
 -- MISSION 3
--- SELECT COUNT(DISTINCT especies_id) FROM opservation;
+ SELECT COUNT(DISTINCT especies_id) FROM observations;
 
 
 -- MISSION 4
--- SELECT COUNT(*) FROM opservation WHERE region_id = 2;
+ SELECT COUNT(*) FROM observations WHERE region_id = 2;
 
 
 -- MISSION 5
--- SELEC COUNT(*)FROM opservation WHERE opservacion_date = '1998-08-08';
+ SELECT COUNT(*)FROM observations WHERE date = '1998-08-08';
 
 
 -- MISSION 6
--- SELECT region_id COUNT(*) FROM opservations GROUP BY region_id ORDEN BY COUNT(*) DESC LIMIT 1;
+ SELECT region_id COUNT(*) FROM observations GROUP BY region_id ORDEN BY COUNT(*) DESC LIMIT 1;
 
 
 -- MISSION 7
--- SELECT especies_id COUNT(*) FROM opservations GRUPO BY especies_id COUNT(*) DESC LIMIT 1;
+ SELECT especies_id COUNT(*) FROM observations GRUPO BY especies_id COUNT(*) DESC LIMIT 1;
 
 -- He usado GROUP BY para organizar los datos por categorías y HAVING para filtrar los resultados que daban los conteos, fue lo que mas me costo 
 -- MISSION 8
--- SELECT region_id COUNT(*) FROM opservations GROUP BY region_id HAVING COUNT(*) > 100;
+ SELECT region_id COUNT(*) FROM observations GROUP BY region_id HAVING COUNT(*) > 5;
+
+-- MISION 9 registros por cada informasion 
+SELECT observer, COUNT() FROM opservations GROUP BY observer ORDER BY COUNT() DESC;
+
+-- mision 10 unir tablas de observaciones uso JOIN para vincular el nombre de la region 
+SELECT observations.*, regions.name
+FROM observations
+JOIN regions ON observations.region_id = regions.id;
+
+--mision 11 vinculacion de tablas 
+SELECT observations.*, species.scientific_name
+FROM observations
+JOIN species ON observations.species_id = species.id;
+
+--mision 12 se agrupa para ver conteos 
+SELECT region_id, species_id, COUNT()
+FROM observations
+GROUP BY region_id, species_id
+ORDER BY COUNT() DESC;
+
+-- mision 13 insertar datos de pruebas 
+INSERT INTO observations (region_id, species_id, date, observer)
+VALUES (1, 1, '2026-03-07', 'Juan');
+
+-- mision 14 corregir y borrar datos 
+UPDATE species SET scientific_name = 'Panthera leo' WHERE id = 1;
+DELETE FROM observations WHERE id = 1;
+
+--mision 15 borrar registros de pruebas 
+DELETE FROM observations WHERE id = 1;
+
